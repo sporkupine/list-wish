7ecd655f0af3a0375b8d9e6b6f1d6daf1ad6d08f
sporkupine <coffeechief25@gmail.com>
2022-03-11 16:30:46 -0600

    create User, add to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_221745) do
+ActiveRecord::Schema.define(version: 2022_03_11_222857) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_03_11_221745) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price"
     t.string "seller"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
