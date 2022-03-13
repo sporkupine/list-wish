de52d5533bbea45135cce3e4592102bb0476cc09
sporkupine <coffeechief25@gmail.com>
2022-03-12 19:03:17 -0600

    added pages, products will now display by id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence :true, length: { minimum: 4, maximum: 50 }
-  validates :seller, presence :true, length: { minimum: 4, maximum: 50}
-  validates :price, presence :true
+  validates :name, presence:true, length: { minimum: 4, maximum: 50 }
+  validates :seller, presence:true, length: { minimum: 4, maximum: 50}
+  validates :price, presence:true
 end
