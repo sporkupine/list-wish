9f2a48411a9fcbc100f29369d42251ca1c6eb5db
sporkupine <coffeechief25@gmail.com>
2022-03-14 18:37:16 -0500

    added edit, update, destroy to products_controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,4 +20,25 @@ class ProductsController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+  
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(params.require(:product).permit(:name, :price, :seller))
+      flash[:notice] = "Product updated!"
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 end
