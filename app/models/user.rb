2c320ef76584f4d8666d5a5973eee18f40a5ba05
sporkupine <coffeechief25@gmail.com>
2022-03-28 18:08:48 -0500

    add secure password to User with bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :products
-
+  has_secure_password
   validates :username, uniqueness: true, presence: true, length: {minimum: 4, maximum: 30}
   validates :email, uniqueness: true, presence: true, length: {minimum: 6, maximum: 50}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_224735) do
+ActiveRecord::Schema.define(version: 2022_03_28_230315) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_224735) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
