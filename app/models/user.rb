568b937736b0a5e8f8f4a2f817c84a47ec2a3582
sporkupine <coffeechief25@gmail.com>
2022-03-28 17:42:31 -0500

    delete users model, remove from db

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence:true, length: { minimum: 4, maximum: 15 }
-  validates :password, presence:true, length: { minimum: 8, maximum: 20 }
-end
