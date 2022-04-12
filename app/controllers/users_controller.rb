17a50f903ff6b463d34a5c85ca68ef2916b54677
sporkupine <coffeechief25@gmail.com>
2022-04-11 20:08:48 -0500

    fix bug on user form when errors present

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save 
-      flash[:notice] = "Account created successfully!"
+      flash[:notice] = "Account created successfully. Welcome to WishList, #{@user.username}!"
       redirect_to @user
     else
         render :new
