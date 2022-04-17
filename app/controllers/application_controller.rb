105237bf017718d7ca4dad7dff3ff56c21984e42
sporkupine <coffeechief25@gmail.com>
2022-04-16 21:38:56 -0500

    fix active-link errors (mostly)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,15 @@ class ApplicationController < ActionController::Base
   # def require_same_user
 
   # end
+
+  # def add_product
+  #   if !helper.logged_in?
+  #     flash[:notice] = "You have to log in to do that!"
+  #     redirect_to login_path
+  #   else
+  #     user.products.
+  #                   ^ whatever function we use to add products to fav lists
+  #                     will go here
+  #   end
+  # end
 end
