0e0656abfcc3b9c95790ce344e550811115779f3
sporkupine <coffeechief25@gmail.com>
2022-04-18 14:39:30 -0500

    add description to products show and form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -64,7 +64,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :seller, :img_path)
+    params.require(:product).permit(:name, :price, :seller, :description, :img_path)
   end
 
 end
