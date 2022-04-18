4b39c07a79be068e44a01a5425e173affde504dc
sporkupine <coffeechief25@gmail.com>
2022-04-18 14:06:06 -0500

    add description to product model and db

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,6 @@ class Product < ApplicationRecord
 
   validates :name, presence:true, length: { minimum: 4, maximum: 50 }
   validates :seller, presence:true, length: { minimum: 4, maximum: 50}
+  validates :description, length: { minimum: 0, maximum: 150 }
   validates :price, presence:true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_044912) do
+ActiveRecord::Schema.define(version: 2022_04_18_190206) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_04_17_044912) do
     t.float "price"
     t.string "seller"
     t.string "img_path", default: "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+    t.string "description", default: ""
   end
 
   create_table "user_products", force: :cascade do |t|
